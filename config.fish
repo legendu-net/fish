007b2d88133f6b083e4b91ece11e7f2b381ae90e
Ben Du <longendu@yahoo.com>
2026-04-06 11:52:05 -0700

    add abbr

--- config.fish
+++ config.fish
@@ -36,6 +36,7 @@ if status is-interactive
     abbr --add gc git commit -m
     abbr --add gitdiff git diff
     abbr --add gd git diff
+    abbr --add gdni git diff --no-index
     abbr --add gitfetch git fetch
     abbr --add gfo git fetch origin
     abbr --add gitrestore git restore
