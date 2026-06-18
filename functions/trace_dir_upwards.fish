c0a9aef8d0a11876e20ea7d425898d580a0590f7
Ben Du <longendu@gmail.com>
2026-06-17 20:35:20 -0700

    fix(trace_dir_upwards): set empty stem instead of exiting with error at root

--- functions/trace_dir_upwards.fish
+++ functions/trace_dir_upwards.fish
@@ -10,9 +10,15 @@ function trace_dir_upwards --description 'Trace directories upwards until target
         return 0
     end
 
+    if test (count $argv) -ne 2
+        echo (set_color $fish_color_error)"Error: trace_dir_upwards requires exactly 2 arguments."(set_color normal) >&2
+        _trace_dir_upwards_usage >&2
+        return 1
+    end
+
     set -l dir "$argv[1]"
-    set -l name "$argv[2]"
-    set -l stem (path basename "$dir")
+    set -l name (path basename -- "$argv[2]")
+    set -l stem (path basename -- "$dir")
 
     while test "$stem" != "$name"
         if contains -- "$stem" / ""
@@ -20,14 +26,13 @@ function trace_dir_upwards --description 'Trace directories upwards until target
             return 1
         end
 
-        set -l parent (path dirname "$dir")
+        set -l parent (path dirname -- "$dir")
         if test "$parent" = "$dir"
-            echo (set_color $fish_color_error)"Error: $name is not found in $argv[1]!"(set_color normal) >&2
-            return 1
+            set stem ""
+        else
+            set dir "$parent"
+            set stem (path basename -- "$dir")
         end
-
-        set dir "$parent"
-        set stem (path basename "$dir")
     end
 
     echo "$dir"
